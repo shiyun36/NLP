b66be74855c59879bfd2b8c13196e8af942479e0
shiyun36 <111891177+shiyun36@users.noreply.github.com>
2024-05-26 22:33:36 +0200

    Update src.py

--- rnn/src.py
+++ rnn/src.py
@@ -7,6 +7,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
+import re
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 device
@@ -18,22 +19,53 @@ test = pd.read_csv('test.csv')
 
 train.shape, validation.shape, test.shape
 
-import re
-
 def preprocess_text(text):
-    # Check if the text is a string
     if not isinstance(text, str):
         return []
-    
-    # Keep only letters and whitespaces
     pattern = f"[a-zA-Z\s]"
     text = ''.join(re.findall(pattern, text))
-    
-    # Convert to lowercase
     text = text.lower()
-    
-    # Tokenize the text
     tokens = nltk.word_tokenize(text)
 #     tokens = ' '.join(tokens)
-    
     return tokens
+
+#apply the preprocess text to 
+train['user_review'] = train['user_review'].apply(preprocess_text)
+validation['user_review'] = validation['user_review'].apply(preprocess_text)
+test['user_review'] = test['user_review'].apply(preprocess_text)
+
+train.head()
+
+def build_vocabulary(reviews):
+    vocab = {}
+    index = 1  # Start indexing from 1; reserve 0 for padding
+    for review in reviews:
+        for word in review:
+            if word not in vocab:
+                vocab[word] = index
+                index += 1
+    return vocab
+
+# Concatenate all reviews to build the vocabulary
+all_reviews = train['user_review'].tolist() + validation['user_review'].tolist() + test['user_review'].tolist()
+vocab = build_vocabulary(all_reviews)
+print("Vocabulary Length:", len(vocab))
+first_50 = list(vocab.items())[:50]
+for key, value in first_50:
+    print(f'{key}: {value}')
+
+
+#Indexing reviews based on the vocabulary
+def index_and_pad_reviews(reviews, vocab, max_length=100):
+    """Index and pad tokenized reviews to a fixed length."""
+    indexed_reviews = []
+    for review in reviews:
+        indexed_review = [vocab.get(word, 0) for word in review]  # Use vocab.get to handle unknown words
+        # Truncate if review length exceeds max_length
+        truncated_review = indexed_review[:max_length]
+        # Pad review with zeros if it's shorter than max_length
+        padded_review = truncated_review + [0] * (max_length - len(truncated_review))
+        indexed_reviews.append(padded_review)
+    return indexed_reviews
+
+
